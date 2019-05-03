ae45a04a611f20b9ab7ddb9eba76977d27906544
Sidnei Pacheco <sidneip.junior@gmail.com>
2019-05-03 15:58:19 -0300

    env development

--- lib/octadesk_api.rb
+++ lib/octadesk_api.rb
@@ -4,7 +4,10 @@ require "octadesk_api/client"
 
 module OctadeskApi
   class << self
-    attr_accessor :access_key
+    attr_accessor :access_key, :env
+    def production?
+      ENV['RAILS_ENV'] == 'production' || self.env == 'production'
+    end
   end
   class Error < StandardError; end
   # Your code goes here...
